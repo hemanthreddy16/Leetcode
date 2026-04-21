7f8d97cf3985ef28f54aa501b70874e0e9d7a838
Hemanth Reddy <reddamhemanth@gmail.com>
2026-04-21 21:11:23 +0530

    Time: 1 ms (99.98%), Space: 47 MB (31.8%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
         for(int num : nums){
-            result = result ^ num;
+            result = result ^ num;//a^a=0,0^b=b;
         }
         return result;
             }
