8c5233f3efe62fe6aa5a63209ae97f77994bb000
Hemanth Reddy <reddamhemanth@gmail.com>
2026-08-11 16:33:16 +0530

    Time: 1 ms (77.27%), Space: 46.6 MB (63.89%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,45 +1,13 @@
-import java.util.HashSet;
-
-// Solution class containing removeDuplicates method
 class Solution {
-    // Removes duplicates using HashSet and returns count of unique elements
     public int removeDuplicates(int[] nums) {
-        // HashSet to store unique elements we have seen
-        HashSet<Integer> seen = new HashSet<>();
-
-        // Position to overwrite next unique element
-        int index = 0;
-
-        // Loop over each number in nums
-        for (int num : nums) {
-            // If num is not in the set, it is unique
-            if (!seen.contains(num)) {
-                // Add num to the set
-                seen.add(num);
-
-                // Write num at current index position
-                nums[index] = num;
-
-                // Move index forward
-                index++;
+        int n=nums.length;
+        int i=0;
+        for(int j=1;j<n;j++){
+            if(nums[j]!=nums[i]){
+                nums[i+1]=nums[j];
+                i++;
             }
         }
-        // Return number of unique elements
-        return index;
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
-
-        Solution sol = new Solution();
-        int k = sol.removeDuplicates(nums);
-
-        System.out.println("k = " + k);
-        System.out.print("Array after removing duplicates: ");
-        for (int i = 0; i < k; i++) {
-            System.out.print(nums[i] + " ");
-        }
+        return (i+1);
     }
 }
